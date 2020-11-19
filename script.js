a94acabdacaf66f02a6128960f68e9b39d0fd7d7
carlos-douglas-dev <carlos.douglas.dev@gmail.com>
2020-11-19 13:06:05 -0400

    definir direcoes e impedir que a cobrinha suma

--- script.js
+++ script.js
@@ -20,5 +20,45 @@ function criarCobrinha() {
   }
 }
 
-criarBG();
-criarCobrinha();
+document.addEventListener('keydown', update);
+
+function update (event){
+  if(event.keycode == 37 && direction != "right") direction = "left";
+  if(event.keycode == 38 && direction != "down") direction = "up";
+  if(event.keycode == 39 && direction != "left") direction = "right";
+  if(event.keycode == 40 && direction != "up") direction = "down";
+}
+
+function iniciarJogo(){
+  if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+  if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+  criarBG();
+  criarCobrinha();
+
+  //cria o posicionamento da cobrinha pra se ter um ponto de partida
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  //criar as coordenadas que ela pode ir
+  if(direction == "right") snakeX += box;
+  if(direction == "left") snakeY -= box;
+  if(direction == "up") snakeY -= box;
+  if(direction == "down") snakeY += box;
+
+  //retira o ultimo elemento do nosso Array
+  snake.pop();
+
+  //acrescenta 
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead);
+
+}
+//setInterval: atualiza a tela em um determinado intervalo de tempo
+let jogo = setInterval(iniciarJogo, 100);
