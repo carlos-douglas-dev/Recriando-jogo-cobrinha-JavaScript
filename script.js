10505ff6551c59642700a102f91186f8475eb48c
carlos-douglas-dev <carlos.douglas.dev@gmail.com>
2020-11-19 22:20:43 -0400

    definir fim do jogo

--- script.js
+++ script.js
@@ -32,10 +32,10 @@ function drawFood(){
 document.addEventListener("keydown", update);
 
 function update (event){
-  if(event.keycode == 37 && direction != "right") direction = "left";
-  if(event.keycode == 38 && direction != "down") direction = "up";
-  if(event.keycode == 39 && direction != "left") direction = "right";
-  if(event.keycode == 40 && direction != "up") direction = "down";
+  if(event.keyCode == 37 && direction != "right") direction = "left";
+  if(event.keyCode == 38 && direction != "down") direction = "up";
+  if(event.keyCode == 39 && direction != "left") direction = "right";
+  if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
 function iniciarJogo(){
@@ -43,6 +43,14 @@ function iniciarJogo(){
   if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
   if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
   if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+  for(i = 1; i < snake.length; i++){
+    if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+      //para a função jogo
+      clearInterval(jogo);
+      alert('Game Over :( ');
+    }
+  }
 
   criarBG();
   criarCobrinha();
